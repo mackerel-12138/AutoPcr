b59437acb96033e835e394224b9109037e7068d4
mackerel-12138 <zlj12138@gmail.com>
2023-04-09 10:44:15 +0800

    update build.bat

--- AutoPcr_py/build.bat
+++ AutoPcr_py/build.bat
@@ -6,9 +6,9 @@ md TMP\img
 
 echo. 复制资源
 XCOPY  .\AutoPcr4.0.py .\TMP\ /Y
-XCOPY  .\AutoPcr4.0_GUI.py .\TMP\ /Y
-XCOPY  .\AutoPcr.spec .\TMP\ /Y
 XCOPY  .\AutoPcr4.0.spec .\TMP\ /Y
+XCOPY  .\AutoPcr4.0_GUI.py .\TMP\ /Y
+XCOPY  .\AutoPcr4.0_GUI.spec .\TMP\ /Y
 XCOPY  .\CloseLeiDian.cmd .\TMP\ /Y
 XCOPY  .\config.ini .\TMP\ /Y
 XCOPY  .\StartCmd.cmd .\TMP\ /Y
@@ -23,19 +23,21 @@ pyinstaller  .\AutoPcr4.0.spec
 MOVE .\dist\AutoPcrCmd.exe .\
 
 echo. 打包exe
-pyinstaller   .\AutoPcr.spec
+pyinstaller   .\AutoPcr4.0_GUI.spec
 MOVE .\dist\AutoPcr.exe .\
 
 echo. 打包zip
 rd /s /q .\build
 rd /s /q .\dist
-del .\AutoPcr.spec
-del .\AutoPcr4.0.spec
 del .\AutoPcr4.0.py
+del .\AutoPcr4.0.spec
 del .\AutoPcr4.0_GUI.py
+del .\AutoPcr4.0_GUI.spec
 
 cd ..
 python -m zipfile -c AutoPcr.zip .\TMP\AutoPcr.exe .\TMP\AutoPcrCmd.exe .\TMP\CloseLeiDian.cmd .\TMP\config.ini .\TMP\StartCmd.cmd .\TMP\StartLeiDian.cmd .\TMP\StartLeiDian1.cmd .\TMP\com.bilibili.priconne_960x540.kmp .\TMP\img\
 
 echo. 删除临时文件夹
 rd /s /q .\TMP
+
+timeout \t 60
